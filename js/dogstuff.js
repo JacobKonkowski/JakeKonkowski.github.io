359e9f14cf29689d5466c8ee1c70ebaaca3ef9d1
Jake Konkowski <jrkonkowski-social@protonmail.com>
2023-01-24 21:30:12 -0500

    Updated Bootstrap and jQuery, Removed unused JavaScript

--- js/dogstuff.js
+++
@@ -1,6 +0,0 @@
-$("#dogButton").click(function(){
-  $.get("https://dog.ceo/api/breeds/image/random").done(function(data){
-    console.log(data.message);
-    $("#dogPic").attr("src", data.message);
-  });
-});
